9c584334e14a2a00e34f0b3362bb32ea44b4e7d3
Alexander Shchapov <sasha@imedicare.com>
2016-08-03 14:14:09 +0300

    Bump version.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst') as f:
 
 setup(
     name='python-callfire',
-    version='0.1.0',
+    version='0.9.0',
     description='CallFire API thin wrapper.',
     long_description=readme,
     author='Alexander Shchapov',
