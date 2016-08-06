85c090301f0328a4b3d60685b11ad45f848b37d4
Alexander Shchapov <sasha@imedicare.com>
2016-08-06 13:41:43 +0300

    Add license.

--- setup.py
+++ setup.py
@@ -6,11 +6,15 @@ from setuptools import setup, find_packages
 with open('README.rst') as f:
     readme = f.read()
 
+with open('LICENSE') as f:
+    license = f.read()
+
 setup(
     name='python-callfire',
     version='0.9.3',
     description='CallFire API thin wrapper.',
     long_description=readme,
+    license=license,
     author='Alexander Shchapov',
     author_email='sasha@imedicare.com',
     packages=find_packages(exclude=('tests', 'swagger'))
