1eefa0d07c357fc9d63cb0de6b3473ceefaa9b23
Alexander Shchapov <sasha@imedicare.com>
2016-08-18 12:19:20 +0300

    Update setup.py.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     install_requires=['six'],
     tests_require=['nose', 'flexmock'],
     test_suite='tests',
-    packages=find_packages(exclude=('tests', 'swagger')),
+    packages=find_packages(exclude=('tests', 'swagger', '.travis.yml')),
     package_data={'': ['LICENSE']},
     classifiers=(
         'Development Status :: 4 - Beta',
